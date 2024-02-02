41c400793f82c101d3852fa24a1193747a758b31
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2024-02-02 15:11:16 -0300

    Button Start / Stop

--- RevitAddin.VisualStudioDebug/Revit/App.cs
+++ RevitAddin.VisualStudioDebug/Revit/App.cs
@@ -1,6 +1,9 @@
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using RevitAddin.VisualStudioDebug.Services;
 using ricaun.Revit.UI;
+using ricaun.Revit.UI.Drawing;
 using System;
 
 namespace RevitAddin.VisualStudioDebug.Revit
@@ -9,19 +12,93 @@ namespace RevitAddin.VisualStudioDebug.Revit
     public class App : IExternalApplication
     {
         private static RibbonPanel ribbonPanel;
+        private static VisualStudioDebugAttach visualStudioDebugAttach;
         public Result OnStartup(UIControlledApplication application)
         {
-            ribbonPanel = application.CreatePanel("VisualStudioDebug");
-            ribbonPanel.CreatePushButton<Commands.Command>()
-                .SetLargeImage(Properties.Resources.Revit.GetBitmapSource());
+            //Console.WriteLine(application);
+            //visualStudioDebugAttach = new VisualStudioDebugAttach();
+
+            ribbonPanel = application.CreatePanel("Debug");
+
+            //var button = ribbonPanel.CreatePushButton<Commands.Command>()
+            //    .SetLargeImage(Properties.Resources.Revit.GetBitmapSource());
+
+            //ribbonPanel.SetDialogLauncher(button);
+
+            ribbonPanel.RowStackedItems(
+                ribbonPanel.CreatePushButton<CommandPlay>("Start").SetLargeImage("Resources/Play-Light.ico"),
+                //ribbonPanel.CreatePushButton<CommandPause>("Pause").SetLargeImage("Resources/Pause-Light.ico"),
+                ribbonPanel.CreatePushButton<CommandStop>("Stop").SetLargeImage("Resources/Stop-Light.ico")
+            );
+
             return Result.Succeeded;
         }
 
         public Result OnShutdown(UIControlledApplication application)
         {
             ribbonPanel?.Remove();
+            visualStudioDebugAttach?.Dispose();
             return Result.Succeeded;
         }
+
+
+        [Transaction(TransactionMode.Manual)]
+        public class CommandPlay : IExternalCommand, IExternalCommandAvailability
+        {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elementSet)
+            {
+                UIApplication uiapp = commandData.Application;
+                visualStudioDebugAttach?.Dispose();
+                visualStudioDebugAttach = new VisualStudioDebugAttach();
+
+                return Result.Succeeded;
+            }
+
+            public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+            {
+                return System.Diagnostics.Debugger.IsAttached == false;
+            }
+        }
+
+        [Transaction(TransactionMode.Manual)]
+        public class CommandPause : IExternalCommand, IExternalCommandAvailability
+        {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elementSet)
+            {
+                UIApplication uiapp = commandData.Application;
+
+                VisualStudioDebugUtils.DTE.Debugger.Break();
+
+                return Result.Succeeded;
+            }
+
+            public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+            {
+                return visualStudioDebugAttach is not null;
+            }
+        }
+
+        [Transaction(TransactionMode.Manual)]
+        public class CommandStop : IExternalCommand, IExternalCommandAvailability
+        {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elementSet)
+            {
+                UIApplication uiapp = commandData.Application;
+
+                VisualStudioDebugUtils.DTE.Debugger.Stop();
+
+                visualStudioDebugAttach?.Dispose();
+                visualStudioDebugAttach = null;
+
+                return Result.Succeeded;
+            }
+
+            public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+            {
+                return System.Diagnostics.Debugger.IsAttached;
+            }
+        }
+
     }
 
 }
--- RevitAddin.VisualStudioDebug/Revit/Commands/CommandError.cs
+++ RevitAddin.VisualStudioDebug/Revit/Commands/CommandError.cs
@@ -1,23 +1,19 @@
-using Autodesk.Revit.Attributes;
+﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using RevitAddin.VisualStudioDebug.Services;
-using System;
-using System.Diagnostics;
 
 namespace RevitAddin.VisualStudioDebug.Revit.Commands
 {
     [Transaction(TransactionMode.Manual)]
-    public class Command : IExternalCommand
+    public class CommandError : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elementSet)
         {
             UIApplication uiapp = commandData.Application;
 
-            using (new VisualStudioDebugAttach())
-            {
-                Console.WriteLine(uiapp);
-            }
+            int i = 1;
+            int j = 0;
+            int k = i / j;
 
             return Result.Succeeded;
         }
