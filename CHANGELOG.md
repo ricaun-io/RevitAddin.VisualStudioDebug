a0c3d36a6005edab7f3104cb597c75e647a71262
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2024-03-13 17:50:49 -0300

    Add event feature

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.1.0] / 2024-03-13
+- Add event feature
+
 ## [1.0.0] / 2024-02-01
 - Add Icons
 - Add Button Start / Stop
@@ -12,4 +15,5 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - First Release
 
 [vNext]: ../../compare/1.0.0...HEAD
+[1.1.0]: ../../compare/1.0.0...1.1.0
 [1.0.0]: ../../compare/1.0.0
--- RevitAddin.VisualStudioDebug/Revit/App.cs
+++ RevitAddin.VisualStudioDebug/Revit/App.cs
@@ -29,23 +29,50 @@ namespace RevitAddin.VisualStudioDebug.Revit
                 .SetLargeImage("Resources/Play-Light.ico")
                 .SetToolTip("Start Debugging using Visual Studio process.");
 
+            var eventButton = ribbonPanel.CreatePushButton<CommandEvent>("Event")
+                .SetLargeImage("Resources/Pause-Light.ico")
+                .SetToolTip("Start Debugging using Visual Studio process when AssemblyLoad event happen.");
+
             var stopButton = ribbonPanel.CreatePushButton<CommandStop>("Stop")
                 .SetLargeImage("Resources/Stop-Light.ico")
                 .SetToolTip("Stop Debugging using Visual Studio process.");
 
             ribbonPanel.RowStackedItems(
                 startButton,
+                eventButton,
                 //ribbonPanel.CreatePushButton<CommandPause>("Pause").SetLargeImage("Resources/Pause-Light.ico"),
                 stopButton
             );
 
+            AppDomain.CurrentDomain.AssemblyLoad += CurrentDomain_AssemblyLoad;
+
             return Result.Succeeded;
+        }
+
+        private void CurrentDomain_AssemblyLoad(object sender, AssemblyLoadEventArgs args)
+        {
+            if (EventLoad)
+            {
+                EventLoad = false;
+                visualStudioDebugAttach?.Dispose();
+                visualStudioDebugAttach = new VisualStudioDebugAttach();
+            }
+        }
+
+        private static bool EventLoad;
+
+        private static void EventMonitor()
+        {
+            EventLoad = !EventLoad;
         }
 
         public Result OnShutdown(UIControlledApplication application)
         {
             ribbonPanel?.Remove();
             visualStudioDebugAttach?.Dispose();
+
+            AppDomain.CurrentDomain.AssemblyLoad -= CurrentDomain_AssemblyLoad;
+
             return Result.Succeeded;
         }
 
@@ -58,6 +85,24 @@ namespace RevitAddin.VisualStudioDebug.Revit
                 UIApplication uiapp = commandData.Application;
                 visualStudioDebugAttach?.Dispose();
                 visualStudioDebugAttach = new VisualStudioDebugAttach();
+
+                return Result.Succeeded;
+            }
+
+            public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+            {
+                return System.Diagnostics.Debugger.IsAttached == false && EventLoad == false;
+            }
+        }
+
+        [Transaction(TransactionMode.Manual)]
+        public class CommandEvent : IExternalCommand, IExternalCommandAvailability
+        {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elementSet)
+            {
+                UIApplication uiapp = commandData.Application;
+
+                EventMonitor();
 
                 return Result.Succeeded;
             }
